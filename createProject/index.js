9370a04204341e1c7257b69e9a143be215ce5752
Michael Randall <michael.j.randall@usace.army.mil>
2021-05-24 21:01:02 -0700

    working on replace function for CRUD operations

--- createProject/index.js
+++ createProject/index.js
@@ -2,31 +2,25 @@ const config = require("../sharedCode/config");
 const { CosmosClient } = require("@azure/cosmos");
 
 module.exports = async function (context, req) {
-    const endpoint = config.endpoint;
-    const key = config.key;
-    const client = new CosmosClient({ endpoint, key });
+  const endpoint = config.endpoint;
+  const key = config.key;
+  const client = new CosmosClient({ endpoint, key });
 
+  const database = client.database(config.databaseId);
+  const container = database.container(config.containerId);
 
-    const database = client.database(config.databaseId);
-    const container = database.container(config.containerId);
+  const project = req.body;
 
-    // test value - uncomment to test
-    // const testitem = {
-    //     "project":"Do some situps, pig",
-    //     "category": "task",
-    //     "completed": false
-    // }
+  const { resource: createdItem } = await container.items.create(project);
 
-    const {resource: createdItem } = await container.items.create(req.body.project);
+  const responseMessage = {
+    status: 200,
+    message: "Ok",
+    data: createdItem,
+  };
 
-    const responseMessage = {
-        "status": 200,
-        "message": "Ok",
-        "data": createdItem
-    }
-    
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
+};
--- updateProject/index.js
+++ updateProject/index.js
@@ -4,32 +4,32 @@ const { CosmosClient } = require("@azure/cosmos");
 // will require data in the form of an object passed in on body
 // will require an id passed in on query
 module.exports = async function (context, req) {
-    const endpoint = config.endpoint;
-    const key = config.key;
-    const client = new CosmosClient({ endpoint, key });
+  const endpoint = config.endpoint;
+  const key = config.key;
+  const client = new CosmosClient({ endpoint, key });
 
-    const database = client.database(config.databaseId);
-    const container = database.container(config.containerId);
+  const database = client.database(config.databaseId);
+  const container = database.container(config.containerId);
 
-    const theId = req.params.id;
-    const theCategory = req.params.category;
-    const updatedProject = req.body;
+  const theId = req.params.id;
+  const { resource: theProject } = await container.item(theId).read();
 
-    updatedProject.project = "Eat some cabbage you pig dog cow";
+  const { id, category } = theProject;
 
-    
+  theProject.project = "Grapple with a bear";
 
-    const {resource: updatedItem } = await container.item(theId, theCategory).replace(updatedProject)
+  const { resource: updatedItem } = await container
+    .item(id, category)
+    .replace(theProject);
 
+  const responseMessage = {
+    status: 200,
+    message: res.message,
+    data: updatedItem,
+  };
 
-    const responseMessage = {
-        "status": 200,
-        "message": res.message,
-        "data": updatedItem
-    }
-    
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
+};
